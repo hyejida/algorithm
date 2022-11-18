f8bfcc622bb40f94ff2eac6791a603999bd92227
hyejida <owlgpwl94@gmail.com>
2022-11-17 23:24:31 -0800

    for_loop

---
+++ baekjoon/2438-stars.js
@@ -0,0 +1,18 @@
+// 📝 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+//🔎 문제분석
+//💡 규칙찾기
+// 𝌣 코딩화
+
+
+
+let output = ''
+
+for(let i = 1; i<=100; i++){
+    for(let j = 0; j<i; j++){
+        output += '*'
+    }
+    output += '\n'
+}
+
+
+console.log(output)
