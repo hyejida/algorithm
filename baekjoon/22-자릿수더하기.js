d06c9aa949e45ce96112796cafb1f013f878c30c
hyejida <owlgpwl94@gmail.com>
2022-11-25 00:37:30 -0800

    Create 22-자릿수더하기.js

---
+++ baekjoon/22-자릿수더하기.js
@@ -0,0 +1,14 @@
+// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+
+function solution(n)
+{
+    // var answer = 0;
+ const arr = n.toString().split('', n.length)
+let arr2 = arr.map(function(element){
+    return Number(element)
+})
+ return arr2.reduce((a,b)=> a+b)
+
+}
